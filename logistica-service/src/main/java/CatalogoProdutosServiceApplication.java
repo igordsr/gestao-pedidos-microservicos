245bc02c046a038b269a863db65c8823020cbad2
Elvis Julius <elvis.olopes@mercadolivre.com>
2024-05-19 20:30:31 -0300

    adicionando logistica-service

---
+++ logistica-service/src/main/java/CatalogoProdutosServiceApplication.java
@@ -0,0 +1,14 @@
+package src.main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CatalogoProdutosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutosServiceApplication.class, args);
+	}
+
+}
