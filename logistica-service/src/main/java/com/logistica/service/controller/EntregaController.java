fffb2586c878369e3f2fb20175e3d1e92a76fb92
Lays Vale <laysnogueira96@gmail.com>
2024-05-20 23:41:49 -0300

    fix

--- logistica-service/src/main/java/com/logistica/service/controller/EntregaController.java
+++ logistica-service/src/main/java/com/logistica/service/controller/EntregaController.java
@@ -8,11 +8,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @RestController()
 @RequestMapping(value = "/entrega", produces = {"application/json"})
@@ -30,16 +31,4 @@ public class EntregaController {
     public ResponseEntity<Map<String, List<EntregaDTO>>> getRelatorioDeEntregas() {
         return new ResponseEntity<>(entregaService.processarPedidosPagosEAgruparPorCep(), HttpStatus.OK);
     }
-
-    @PatchMapping("/{idEntrega}")
-    @Operation(summary = "Confirmar Entrega", description = "Confirma a Entrega", method = "PATCH")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Entrega realizada com sucesso"),
-            @ApiResponse(responseCode = "404", description = "Entrega não encontrada")
-    })
-    public ResponseEntity<Void> confirmaEntrega(@PathVariable UUID idEntrega) {
-        entregaService.atualizarEntrega(idEntrega);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
 }
